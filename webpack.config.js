0b8dcf841aedfce5a8887a79721bffb65624960f
ajfAfg <56056962+ajfAfg@users.noreply.github.com>
2022-08-07 05:20:44 +0900

    add the config

--- webpack.config.js
+++ webpack.config.js
@@ -57,6 +57,10 @@ const reactConfig = {
     path: path.resolve(__dirname, "dist"),
     filename: "index.js",
   },
+  externals: {
+    vscode: "commonjs vscode", // the vscode-module is created on-the-fly and must be excluded. Add other modules that cannot be webpack'ed, 📖 -> https://webpack.js.org/configuration/externals/
+    // modules added here also need to be added in the .vscodeignore file
+  },
   resolve: {
     // support reading TypeScript and JavaScript files, 📖 -> https://github.com/TypeStrong/ts-loader
     extensions: [".ts", ".js", ".tsx"],
