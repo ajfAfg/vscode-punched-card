0d4688ffeaf0dc85643088d196a2f81cc96d4e28
ajfAfg <56056962+ajfAfg@users.noreply.github.com>
2022-08-07 13:08:49 +0900

    update

--- src/App.tsx
+++ src/App.tsx
@@ -35,76 +35,121 @@ export const App: FC = () => {
   //   setBar(state.text);
   // }
 
-  return (
-    <>
-      {punchedCards.map((punchedCard, i) => {
-        return (
-          <div className="flex">
-            <div className="w-[64rem] mx-auto my-4 p-4 bg-amber-50 flex justify-between">
-              <>
-                {punchedCard.map((ascii, j) => {
-                  return (
-                    <div className="flex-col">
-                      {ascii.map((didPunched, k) => (
-                        <div>
-                          <button
-                            className="text-gray-800 text-lg font-mono"
-                            onClick={() => {
-                              // const newPunchedCards = punchedCards.map(
-                              //   (punchedCard) =>
-                              //     punchedCard.map((ascii) => [...ascii])
-                              // ) as PunchedCard[];
-                              const newPunchedCards = clone(punchedCards);
-                              newPunchedCards[i][j][k] = true;
-                              setPunchedCards(newPunchedCards);
-                              //
-                              vscode.current?.postMessage({
-                                type: "change",
-                                text: toText(newPunchedCards.flat()),
-                              });
-                            }}
-                          >
-                            {didPunched ? "x" : k + 1}
-                          </button>
-                        </div>
-                      ))}
-                    </div>
-                  );
-                })}
-              </>
-            </div>
-
-            <button
-              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 h-8 my-auto"
-              onClick={() => {
-                const newPunchedCards = clone(punchedCards);
-                newPunchedCards.splice(i, 1);
-                setPunchedCards(newPunchedCards);
-                //
-                vscode.current?.postMessage({
-                  type: "change",
-                  text: toText(newPunchedCards.flat()),
-                });
-              }}
-            >
-              Remove
-            </button>
-          </div>
-        );
-      })}
+  const AddPunchedCardButton: FC<{ className?: string; index: number }> = ({
+    className = "",
+    index,
+  }) => {
+    return (
+      // <div className="w-8 h-8 rounded-full mx-auto border-2">
+      //   <button
+      //     // className=" my-8 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4"
+      //     className=""
+      //     onClick={() => {
+      //       const newPunchedCards = clone(punchedCards);
+      //       // newPunchedCards.push(fromText("")!);
+      //       // setPunchedCards(newPunchedCards);
+      //       setPunchedCards([
+      //         ...newPunchedCards.slice(0, index),
+      //         fromText("")!,
+      //         ...newPunchedCards.slice(index, newPunchedCards.length),
+      //       ]);
+      //     }}
+      //   >
+      //     {/* Add a punched card */}
+      //   </button>
+      // </div>
       <button
-        className="mx-auto my-8 flex bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4"
+        // className=" my-8 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4"
+        className={
+          "w-8 h-8 rounded-full border-2 border-gray-500 flex mx-auto overflow-visible hover:before:text-xl hover:before:bg-slate-100 hover:before:bg-opacity-30 hover:before:text-slate-300 hover:before:content-['Add_a_punched_card']"
+        }
         onClick={() => {
-          // const newPunchedCards = punchedCards.map((punchedCard) =>
-          //   punchedCard.map((ascii) => [...ascii])
-          // ) as PunchedCard[];
           const newPunchedCards = clone(punchedCards);
-          newPunchedCards.push(fromText("")!);
-          setPunchedCards(newPunchedCards);
+          setPunchedCards([
+            ...newPunchedCards.slice(0, index),
+            fromText("")!,
+            ...newPunchedCards.slice(index, newPunchedCards.length),
+          ]);
         }}
       >
-        Add a punched card
+        {/* Add a punched card */}
+        {/* <p className="overflow-visible">Add a punched card</p> */}
       </button>
+    );
+  };
+
+  return (
+    <>
+      {punchedCards
+        .map((punchedCard, i) => {
+          return (
+            <div className="flex">
+              <div className="w-[64rem] mx-auto my-4 p-4 pt-24 bg-amber-50 flex justify-between cut">
+                <>
+                  {punchedCard.map((ascii, j) => {
+                    return (
+                      <div className="flex flex-col space-y-2">
+                        {ascii.map((didPunched, k) => (
+                          <div>
+                            <button
+                              className="text-gray-800 text-lg font-mono"
+                              onClick={() => {
+                                const newPunchedCards = clone(punchedCards);
+                                newPunchedCards[i][j][k] = true;
+                                setPunchedCards(newPunchedCards);
+                                //
+                                vscode.current?.postMessage({
+                                  type: "change",
+                                  text: toText(newPunchedCards.flat()),
+                                });
+                              }}
+                            >
+                              {didPunched ? (
+                                <div className="w-3 h-8 bg-black border">
+                                  <div className="m-auto text-black">{0}</div>
+                                </div>
+                              ) : (
+                                <div className="w-3 h-8 border">
+                                  <div className="m-auto">{k + 1}</div>
+                                  {/* {k + 1} */}
+                                </div>
+                              )}
+                            </button>
+                          </div>
+                        ))}
+                      </div>
+                    );
+                  })}
+                </>
+              </div>
+
+              <button
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 h-8 my-auto"
+                onClick={() => {
+                  const newPunchedCards = clone(punchedCards);
+                  newPunchedCards.splice(i, 1);
+                  setPunchedCards(newPunchedCards);
+                  //
+                  vscode.current?.postMessage({
+                    type: "change",
+                    text: toText(newPunchedCards.flat()),
+                  });
+                }}
+              >
+                Remove
+              </button>
+            </div>
+          );
+        })
+        .reduce(
+          (acc, elem, i) => (
+            <>
+              {acc} {elem}
+              <AddPunchedCardButton className="flex mx-auto" index={i + 1} />
+            </>
+          ),
+          <AddPunchedCardButton className="flex mx-auto" index={0} />
+        )}
     </>
   );
 };
